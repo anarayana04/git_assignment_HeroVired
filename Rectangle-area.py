b82c867716dcc497eebfc65853005f4332407be2
anarayana04 <anarayana04@gmail.com>
2023-11-19 02:13:01 +0530

    Rectangle area code

---
+++ Rectangle-area.py
@@ -0,0 +1,11 @@
+def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+
+# Given values
+length = 10
+width = 6
+
+# Calculate and print the area
+area = calculate_rectangle_area(length, width)
+print(f"The area of the rectangle with length {length} and width {width} is: {area}")
